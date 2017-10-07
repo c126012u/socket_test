ae3ea76d20d446582c93875aaab198df610c0d12
c126012u <c126012u@comwork.c.oka-pu.ac.jp>
2017-10-07 17:52:43 +0900

    Create sock_trans.py

---
+++ sock_trans.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+import socket
+import json
+
+host = "127.0.0.1" #お使いのサーバーのホスト名を入れます
+port = 5000 #適当なPORTを指定してあげます
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #オブジェクトの作成をします
+
+client.connect((host, port)) #これでサーバーに接続します
+
+obj = {
+"1":{
+    "location":["0.2", "0.2", "0.8"],
+    "motion":"POINTING",
+    "name":"エルモ"
+}
+}
+
+obj_sock = json.dumps(obj)
+
+
+client.send(obj_sock.encode('utf-8')) #適当なデータを送信します（届く側にわかるように）
+
+response = client.recv(4096) #レシーブは適当な2進数にします（大きすぎるとダメ）
+
+print(response)
